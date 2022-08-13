1b64ecace01c73c55eb39bd11ca076c3c6a92639
Joao Gomes <joaoguilherme102030@gmail.com>
2022-08-13 14:57:22 -0300

    Implementando contrato de repository na camada de data

---
+++ lib/features/src/data/repositories/movie_repository_impl.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+
+import '../../domain/entities/movie.dart';
+import '../../domain/repositories/movie_repository.dart';
+import '../../errors/failure.dart';
+import '../datasources/movie_datasource.dart';
+
+class MovieRepositoryImpl implements MovieRepository {
+  late MovieDatasource datasource;
+
+  MovieRepositoryImpl(this.datasource);
+
+  @override
+  Future<Either<Failure, Movie>> getMovieById(int id) async {
+    try {
+      final remoteData = await datasource.getMovieById(id);
+      return Right(remoteData);
+    } on Failure catch (e) {
+      return Left(e);
+    }
+  }
+}
