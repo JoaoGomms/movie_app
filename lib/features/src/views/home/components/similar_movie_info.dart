2b86be733454b177ca96c18ee0f246d3fd574fa1
Joao Gomes <joaoguilherme102030@gmail.com>
2022-08-14 23:33:20 -0300

    Componentizando seçao do card de filmes similares

---
+++ lib/features/src/views/home/components/similar_movie_info.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:movie_app/features/src/domain/entities/movie.dart';
+
+class SimilarMovieInfo extends StatefulWidget {
+  final Movie movie;
+  const SimilarMovieInfo({Key? key, required this.movie}) : super(key: key);
+
+  @override
+  State<SimilarMovieInfo> createState() => _SimilarMovieInfoState();
+}
+
+class _SimilarMovieInfoState extends State<SimilarMovieInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Image.network(
+        widget.movie.imageUrl,
+        height: 70,
+      ),
+      const SizedBox(
+        width: 4,
+      ),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(widget.movie.name,
+              softWrap: true, overflow: TextOverflow.ellipsis),
+          // Text('Ano e Genero')
+        ],
+      ),
+    ]);
+  }
+}
